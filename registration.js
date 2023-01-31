4c7f88f8dc475db3075a57fb203819813b3e31eb
TKartoshka <kretovtimur@gmail.com>
2023-01-31 17:43:35 +0200

    server

--- registration.js
+++ registration.js
@@ -1,14 +1,24 @@
 const button = document.getElementById('button')
 
-function getInfo()
+async function getInfo()
 {
     const registrationInfo = {
         name : document.getElementById('name').value,
         surname : document.getElementById('surname').value,
-        cardNumber : document.getElementById('name').value,
-        cardPassword : document.getElementById('name').value,
+        cardNumber : document.getElementById('cardNumber').value,
+        cardPassword : document.getElementById('cardPassword').value,
+        email : document.getElementById('email').value,
+        emailPassword : document.getElementById('emailPassword').value,
     }
-
+    
+    let ans = await fetch('/api/registration',
+    {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+         body: JSON.stringify(registrationInfo)
+    })
     console.log(registrationInfo);
 }
-button.addEventListener('click', getInfo())
+button.addEventListener('click', getInfo)
--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from 'express';
+import { appendFileSync, readFileSync } from 'fs';
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.static('.'));
+app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.post('/api/registration', (req, res) => {
+  let text = readFileSync('test.txt', 'utf8')
+  if(text.match(req.body.email) != null || req.body.emailPassword.length == 0)
+  {
+    res.sendStatus(500)
+  }else{
+    appendFileSync('test.txt', JSON.stringify(req.body) + '\n')
+    res.sendStatus(201)
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server work');
+});
